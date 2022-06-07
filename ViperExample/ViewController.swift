da179a0210da93d124fa34b941d5fed5c201a019
ckost9n <ckost9n@gmail.com>
2022-06-07 18:18:23 +0300

    Initial Commit

---
+++ ViperExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViperExample
+//
+//  Created by Konstantin on 07.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
